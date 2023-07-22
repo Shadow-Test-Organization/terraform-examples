eaaed0996a631cb5c2d1b707d5d81d7190bc4ed9
Ujjwal <138803882+shadowshot-x-1@users.noreply.github.com>
2023-07-22 12:22:40 +0530

    Update providers.tf

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.51.0"
+    kubernetes = {
+      source = "hashicorp/kubernetes"
     }
   }
 }
