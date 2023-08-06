c3f6638fd17fa87b712694167addaefa4a2fd3d7
Ujjwal Sharma <ujjwal26599@gmail.com>
2023-08-06 21:24:35 +0530

    Update testmodule.tf

--- infrastructure/testmodule.tf
+++ infrastructure/testmodule.tf
@@ -1,3 +1,3 @@
 module "test_module" {
-  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=v1.0.0"
+  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git"
 }
